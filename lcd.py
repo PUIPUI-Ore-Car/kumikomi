a783dff46d003672d517cd7f389b289d71626f0c
donald-shrimp <t0706244@gmail.com>
2021-06-16 14:45:01 +0900

    lcd修正

--- lcd.py
+++ lcd.py
@@ -5,12 +5,12 @@ import socket
 import ipget
 
 #LCD初期化
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x00′,’0x02′])
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x00′,’0x0c’])
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x00′,’0x01′])
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x00′,’0xff’])
+subprocess.run(['i2cset','-y','1','0x3c','0x00','0x02'])
+subprocess.run(['i2cset','-y','1','0x3c','0x00','0x0c'])
+subprocess.run(['i2cset','-y','1','0x3c','0x00','0x01'])
+subprocess.run(['i2cset','-y','1','0x3c','0x00','0xff'])
 
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x00′,’0x80′])
+subprocess.run(['i2cset','-y','1','0x3c','0x00','0x80'])
 
 ip = ipget.ipget()
 wifi_ip = ip.ipaddr(“wlan0”)
@@ -21,17 +21,17 @@ print(eth_ip)
 for tmp1 in range(len(wifi_ip)):
 wifi_ip_part = str(hex(ord(wifi_ip[tmp1])))
 #print(myip_part)
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x40′,wifi_ip_part])
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x40′,’0x20′])
+subprocess.run(['i2cset','-y','1','0x3c','0x40',wifi_ip_part])
+subprocess.run(['i2cset','-y','1','0x3c','0x40','0x20'])
 
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x00′,’0xa0′])
+subprocess.run(['i2cset','-y','1','0x3c','0x00','0xa0'])
 
 for tmp1 in range(len(eth_ip)):
 eth_ip_part = str(hex(ord(eth_ip[tmp1])))
 #print(myip_part)
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x40′,eth_ip_part])
+subprocess.run(['i2cset','-y','1','0x3c','0x40',eth_ip_part])
 
 #テキスト出力
-subprocess.run([‘i2cset’,’-y’,’1′,’0x3c’,’0x40′,’0x20′])
+subprocess.run(['i2cset','-y','1','0x3c','0x40','0x20'])
 
 #print(myip)
