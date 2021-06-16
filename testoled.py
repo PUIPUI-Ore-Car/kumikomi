281ca7e163644130d4602c686c1ed8105b25aa2e
donald-shrimp <t0706244@gmail.com>
2021-06-16 15:57:48 +0900

    ちんぽやｎ

--- testoled.py
+++ testoled.py
@@ -1,11 +1,11 @@
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-
+import sys
 
 i2c = board.I2C()
 oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
-ipadd=input()
+ip= sys.argv 
 
 # Clear display.
 oled.fill(0)
@@ -21,7 +21,7 @@ font2 = ImageFont.truetype("..../usr/share/fonts/opentype/noto/NotoSansCJK-Regul
 
 # Draw the text
 draw.text((0, 0), "うんち！w", font=font, fill=255)
-draw.text((0, 17), "IPアドレス："+ipadd, font=font2, fill=255)
+draw.text((0, 17), "IPアドレス："+ip[1], font=font2, fill=255)
 
 # Display image
 oled.image(image)
