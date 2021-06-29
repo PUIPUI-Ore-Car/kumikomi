dfade54ddd1619258a64f4c8af130a69729d92fd
kazuki19992 <kushida98@gmail.com>
2021-06-29 15:18:45 +0900

    a

--- USB_mount/mount.sh
+++ USB_mount/mount.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "raspberry" | sudo -S mount -o owner,uid=1001,gid=1001,utf8,flush UUID="5E4E-27F2" /var/lib/mod_tile/ajt
+echo "raspberry" | sudo -S mount -o owner,uid=1001,gid=1001,utf8,flush UUID="5E4E-27F2" /media/samba
 
 exit 0
