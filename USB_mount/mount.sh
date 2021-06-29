2572bfd6366ac4b924edbfb5b063b97d51d5808c
kazuki19992 <kushida98@gmail.com>
2021-06-29 15:22:25 +0900

    うんち

--- USB_mount/mount.sh
+++ USB_mount/mount.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "raspberry" | sudo -S mount -o owner,uid=1001,gid=1001,utf8,flush UUID="5E4E-27F2" /media/samba
+echo "raspberry" | sudo -S mount -o owner,uid=1001,gid=1001,utf8,flush UUID="5E4E-27F2" /media/share
 
 exit 0
