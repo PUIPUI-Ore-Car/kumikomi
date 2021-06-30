e7895ec237158dd75b2623e9ce5f09eb2c22b3c0
kazuki19992 <kushida98@gmail.com>
2021-06-30 11:21:15 +0900

    カウント処理

--- pui-count-server/routes/pui.js
+++ pui-count-server/routes/pui.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const fs = require("fs")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,8 +9,22 @@ router.get('/', function(req, res, next) {
 });
 
 // PUI数を加算する
+let puiCount = 0
 router.post('/', (req, res, next) => {
-  console.log(req.body);
+
+  // countLimit回ごとにWebAPIに送信する
+  const countLimit = 10
+
+  // カウント加算
+  puiCount++
+  console.log(puiCount)
+  if(puiCount === countLimit){
+    // APIに送信
+    console.log("送信")
+    puiCount = 0
+  }
+
+  // console.log(req.body);
   res.send({'res':'OK'})
 });
 
