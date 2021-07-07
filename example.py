d92031e5775a2c232da83959a9403c177638114d
kazuki19992 <kushida98@gmail.com>
2021-07-07 14:16:26 +0900

    change

--- example.py
+++ example.py
@@ -8,18 +8,18 @@ GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
 # read data using pin 14
-instance = dht11.DHT11(pin=14)
+instance = dht11.DHT11(pin=18)
 
 try:
 	while True:
-	    result = instance.read()
-	    if result.is_valid():
-	        print("Last valid input: " + str(datetime.datetime.now()))
+		result = instance.read()
+		if result.is_valid():
+			print("Last valid input: " + str(datetime.datetime.now()))
 
-	        print("Temperature: %-3.1f C" % result.temperature)
-	        print("Humidity: %-3.1f %%" % result.humidity)
+			print("Temperature: %-3.1f C" % result.temperature)
+			print("Humidity: %-3.1f %%" % result.humidity)
 
-	    time.sleep(6)
+		time.sleep(6)
 
 except KeyboardInterrupt:
     print("Cleanup")
