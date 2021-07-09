d0d8e3d98e77fa87a6e66b7e9b6d4f30c406358c
kazuki19992 <kushida98@gmail.com>
2021-07-09 19:31:54 +0900

    a

--- jidou_kun.sh
+++ jidou_kun.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-/usr/bin/python3 /home/pi/kumikomi/ipaddr.py &
-/usr/bin/python3 /home/pi/kumikomi/accel.py &
-/usr/bin/python3 /home/pi/kumikomi/distance.py &
-/usr/bin/python3 /home/pi/kumikomi/temp.py &
+/usr/bin/python3 /home/pi/kumikomi/ipaddr.py & /usr/bin/python3 /home/pi/kumikomi/accel.py & /usr/bin/python3 /home/pi/kumikomi/distance.py & /usr/bin/python3 /home/pi/kumikomi/temp.py &
